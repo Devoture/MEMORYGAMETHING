7e78ce73aeb886acb62a4ce52efed2c152a88b56
Maaz <maaz_haq123@hotmail.com>
2018-08-11 23:07:18 -0400

    got buttons lighting up, can detect if user presses right button

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public SpriteRenderer[] colors;
+	public float timeLit;
+	
+	private int colorPicker;
+	private float timeLitCounter;
+	void Start () {
+		
+	}
+	void Update () {
+		if(timeLitCounter > 0) {
+			timeLitCounter -= Time.deltaTime;
+		} else {
+			colors[colorPicker].color = new Color(colors[colorPicker].color.r, colors[colorPicker].color.g, colors[colorPicker].color.b, 0.5f);
+		}
+	}
+
+	public void Begin() {
+		colorPicker = Random.Range(0, colors.Length);
+		colors[colorPicker].color = new Color(colors[colorPicker].color.r, colors[colorPicker].color.g, colors[colorPicker].color.b, 1f);
+		timeLitCounter = timeLit;
+	}
+
+	public void colorClicked(int buttonNum) {
+		if(colorPicker == buttonNum) {
+			Debug.Log("Correct");
+		} else {
+			Debug.Log("Incorrect");
+		}
+	}
+}
---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour {
+	public int buttonNumber;
+
+	private SpriteRenderer spritecolor;
+	private GameManager gm;
+	// Use this for initialization
+	void Start () {
+		spritecolor = GetComponent<SpriteRenderer>();
+		gm = FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+	{
+		spritecolor.color = new Color(spritecolor.color.r, spritecolor.color.g, spritecolor.color.b, 1f);
+	}
+
+
+	void OnMouseUp()
+	{
+		spritecolor.color = new Color(spritecolor.color.r, spritecolor.color.g, spritecolor.color.b, 0.5f);
+		gm.colorClicked(buttonNumber);
+	}
+}
