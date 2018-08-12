682d9fc75bf03e79b695fc8d9ace6133960967f9
Maaz <maaz_haq123@hotmail.com>
2018-08-12 19:05:55 -0400

    fixed minor coding

--- Assets/Scripts/States/StateGameIntro.cs
+++ Assets/Scripts/States/StateGameIntro.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StateGameIntro : GameState {
-	private float m_countdown = 5.0f;
+	private float countdown = 5.0f;
 	public StateGameIntro(GameManager gm):base(gm) { }
 
 	public override void Enter() { }
 	public override void Execute() {
-		if(m_countdown <= 0 || Input.anyKey) {
+		if(countdown <= 0 || Input.anyKey) {
 			gm.NewGameState(gm.stateGameMenu);
 			gm.UpdateFSM(GameStates.MENU);
 		} else {
-			m_countdown -= Time.deltaTime;
+			countdown -= Time.deltaTime;
 		}
 	 }
 	public override void Exit() { }
